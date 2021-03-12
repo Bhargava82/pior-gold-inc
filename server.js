2c7a320ae0e7a49697fa425ba8d11ac81219015d
Bhargava Patel <bhargavapatel82@gmail.com>
2021-03-12 15:40:03 -0500

    set up the server.js.

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/paras-gold", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+const connection = mongoose.connection;
+
+connection.on("connected", () => {
+  console.log("Mongoose successfully connected!");
+});
+
+connection.on("error", (err) => {
+  console.log("Mongoose connection error: ", err);
+});
