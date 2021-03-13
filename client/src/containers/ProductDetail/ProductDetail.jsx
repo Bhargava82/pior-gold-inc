653c56d18276f4a041e335bb682d85a4034eae6c
Bhargava Patel <bhargavapatel82@gmail.com>
2021-03-13 16:48:12 -0500

    added the routes.

---
+++ client/src/containers/ProductDetail/ProductDetail.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ProductDetail = () => {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <h1 className="center-align">Gold Coins and Bars</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductDetail;
